662a0b58030cfa1566d5fe6d8683ca97756ca033
rumon5h <mdrumonkhan48519@gmail.com>
2022-12-19 11:41:56 +0600

    controllers added

--- app.js
+++ app.js
@@ -16,69 +16,7 @@ app.get('/', (req, res, next) => {
 })
 
 // Posting to database
-app.post('/api/v1/product', async(req, res, next) => {
-    // Save or create product
-    try{
-        // If I don't want to change anything
-        const result = await Product.create(req.body);
-        result.logger()
+app.post('/api/v1/product', );
 
-        // If I want to change any properties of the product
-        // const product = await new Product(req.body);
-        // const res = await product.save();
-
-    res.status(200).json({
-        status: 'success',
-        message: 'Successfully saved product',
-        data: result
-    })
-    }
-    catch(error){
-        res.status(404).json({
-            status: 'Failed',
-            message: 'Something went wrong',
-            error: error.message
-        })
-    }
-});
-
-app.get('/api/v1/product', async(req,res, next) => {
-    try {
-        // To get specific product
-        // const product = await Product.find({_id: '639feb8c48f4bf4c6e071b45'});
-
-        // To get all products
-        // const products = await Product.find({});
-
-        // To get only in-stock products
-        // const products = await Product.find({status: {$ne: 'out-of-stock'}});
-
-        // To get only name and quantity 
-        // const products = await Product.find({}, 'name quantity');
-
-        // To get without name and quantity
-        // const products = await Product.find({}, '-name -quantity');
-
-        // Advantage of using mongoose
-        // const products = await Product.where('name').equals(/\w/)
-        // .where('quantity').gt(50).lt(600)
-        // .limit(2).sort({quantity: -1});
-
-        // To get specific one product
-        // const product = await Product.findById('639feb8c48f4bf4c6e071b');
-
-
-        res.status(200).json({
-            status: 'success',
-            message: 'Successfully get the products',
-            data: products
-        })
-    } catch (error) {
-        res.status(400).json({
-            status: 'Failed',
-            message: 'Something went wrong',
-            error: error.message
-        })
-    }
-})
+app.get('/api/v1/product', )
 module.exports = app;
